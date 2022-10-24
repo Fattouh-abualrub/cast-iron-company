6220dd69f9f7cb4e9a5246d1d44797fbd5f35a04
Fattouh Abualrub <fatoh.abualrub@gmail.com>
2022-10-23 22:46:57 -0400

    gallery pages

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
